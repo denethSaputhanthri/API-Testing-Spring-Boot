a8e16322761e799af82d28d8c3cb65dada397847
Deneth Nadeeja <denethsaputhanthri@gmail.com>
2025-12-24 22:35:16 +0530

    initial commit

--- customer_form/src/main/java/edu/icet/controller/CustomerController.java
+++ customer_form/src/main/java/edu/icet/controller/CustomerController.java
@@ -1,15 +1,7 @@
 package edu.icet.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-
 public class CustomerController {
 
-    @GetMapping("/get-name")
-    public String getCustomerName(){
-        return "deneth nadeeja";
-    }
+
 
 }
--- customer_form/src/main/java/edu/icet/model/Customer.java
+++ customer_form/src/main/java/edu/icet/model/Customer.java
@@ -1,13 +1,4 @@
 package edu.icet.model;
 
-import lombok.Data;
-
-@Data
 public class Customer {
-    private String id;
-    private String name;
-    private String address;
-    private String number;
-    private double salary;
-
 }
