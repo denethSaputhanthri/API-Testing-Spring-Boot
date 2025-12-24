9a83a42143c0c063b2b48480e380b28eb48084fd
Deneth Nadeeja <denethsaputhanthri@gmail.com>
2025-12-24 23:55:42 +0530

    post and get API created

--- customer_form/src/main/java/edu/icet/controller/CustomerController.java
+++ customer_form/src/main/java/edu/icet/controller/CustomerController.java
@@ -1,7 +1,27 @@
 package edu.icet.controller;
 
+import edu.icet.model.Customer;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/customer/api")
 public class CustomerController {
+    List<Customer>customerList=new ArrayList<>();
 
+    @GetMapping("/name")
+    public String getCustomerName(){
+        return "name";
+    }
 
-
+    @PostMapping("/add-details")
+    public void addCustomer(@RequestBody Customer customer){
+        customerList.add(customer);
+    }
+    @GetMapping("/get-details")
+    public List<Customer> getCustomerList() {
+        return customerList;
+    }
 }
--- customer_form/src/main/java/edu/icet/model/Customer.java
+++ customer_form/src/main/java/edu/icet/model/Customer.java
@@ -1,4 +1,12 @@
 package edu.icet.model;
 
+import lombok.Data;
+
+@Data
 public class Customer {
+    private String id;
+    private String name;
+    private String address;
+    private String number;
+    private double salary;
 }
